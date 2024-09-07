98ed6c66e64c71fb11fcc81f60e01920250118cf
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2024-09-07 08:35:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,33 @@ import time
 url = "https://github.com/RMKEC111722203119/Deep-Learning/blob/main/exoplanets.csv"
 df = pd.read_csv("exoplanets.csv")
 
+df = df.rename(columns={
+    'kepid':'KepID', 'kepoi_name':'KOIName', 'kepler_name':'KeplerName',
+    'koi_disposition':'ExoplanetArchiveDisposition', 'koi_pdisposition':'DispositionUsingKeplerData',
+    'koi_score':'DispositionScore', 'koi_fpflag_nt':'NotTransit-LikeFalsePositiveFlag',
+    'koi_fpflag_ss':'koi_fpflag_ss', 'koi_fpflag_co':'CentroidOffsetFalsePositiveFlag',
+    'koi_fpflag_ec':'EphemerisMatchIndicatesContaminationFalsePositiveFlag',
+    'koi_period':'OrbitalPeriod[days', 'koi_period_err1':'OrbitalPeriodUpperUnc.[days',
+    'koi_period_err2':'OrbitalPeriodLowerUnc.[days', 'koi_time0bk':'TransitEpoch[BKJD',
+    'koi_time0bk_err1':'TransitEpochUpperUnc.[BKJD', 'koi_time0bk_err2':'TransitEpochLowerUnc.[BKJD',
+    'koi_impact':'ImpactParamete', 'koi_impact_err1':'ImpactParameterUpperUnc',
+    'koi_impact_err2':'ImpactParameterLowerUnc', 'koi_duration':'TransitDuration[hrs',
+    'koi_duration_err1':'TransitDurationUpperUnc.[hrs', 'koi_duration_err2':'TransitDurationLowerUnc.[hrs',
+    'koi_depth':'TransitDepth[ppm', 'koi_depth_err1':'TransitDepthUpperUnc.[ppm',
+    'koi_depth_err2':'TransitDepthLowerUnc.[ppm', 'koi_prad':'PlanetaryRadius[Earthradii',
+    'koi_prad_err1':'PlanetaryRadiusUpperUnc.[Earthradii', 'koi_prad_err2':'PlanetaryRadiusLowerUnc.[Earthradii',
+    'koi_teq':'EquilibriumTemperature[K', 'koi_teq_err1':'EquilibriumTemperatureUpperUnc.[K',
+    'koi_teq_err2':'EquilibriumTemperatureLowerUnc.[K', 'koi_insol':'InsolationFlux[Earthflux',
+    'koi_insol_err1':'InsolationFluxUpperUnc.[Earthflux', 'koi_insol_err2':'InsolationFluxLowerUnc.[Earthflux',
+    'koi_model_snr':'TransitSignal-to-Nois', 'koi_tce_plnt_num':'TCEPlanetNumbe',
+    'koi_tce_delivname':'TCEDeliver', 'koi_steff':'StellarEffectiveTemperature[K',
+    'koi_steff_err1':'StellarEffectiveTemperatureUpperUnc.[K', 'koi_steff_err2':'StellarEffectiveTemperatureLowerUnc.[K',
+    'koi_slogg':'StellarSurfaceGravity[log10(cm/s**2)', 'koi_slogg_err1':'StellarSurfaceGravityUpperUnc.[log10(cm/s**2)',
+    'koi_slogg_err2':'StellarSurfaceGravityLowerUnc.[log10(cm/s**2)', 'koi_srad':'StellarRadius[Solarradii',
+    'koi_srad_err1':'StellarRadiusUpperUnc.[Solarradii', 'koi_srad_err2':'StellarRadiusLowerUnc.[Solarradii',
+    'ra':'RA[decimaldegrees', 'dec':'Dec[decimaldegrees', 'koi_kepmag':'Kepler-band[mag]'
+})
+
 # Data preprocessing
 df['ExoplanetCandidate'] = df['DispositionUsingKeplerData'].apply(lambda x: 1 if x == 'CANDIDATE' else 0)
 df['ExoplanetConfirmed'] = df['ExoplanetArchiveDisposition'].apply(lambda x: 2 if x == 'CONFIRMED' else 1 if x == 'CANDIDATE' else 0)
